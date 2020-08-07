51ce95cf1498cce3d2af5b315ad802831b49aaa7
Siddharth Nalwaya <siddnlw@gmail.com>
2020-08-07 16:32:23 +0530

    Updates

---
+++ precache-manifest.1a43b3e8727eaf461ae2507dc67f7952.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc517289cd84016c9c41790ef9021a8",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "0fefd6ee71343f1d39bc",
+    "url": "/covid-19-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1272e292d0c4b030081",
+    "url": "/covid-19-tracker/static/css/main.26f4b1eb.chunk.css"
+  },
+  {
+    "revision": "0fefd6ee71343f1d39bc",
+    "url": "/covid-19-tracker/static/js/2.4d8d4ac8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19-tracker/static/js/2.4d8d4ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1272e292d0c4b030081",
+    "url": "/covid-19-tracker/static/js/main.4f3a199a.chunk.js"
+  },
+  {
+    "revision": "8bb060aa26e6348928e9",
+    "url": "/covid-19-tracker/static/js/runtime-main.0855a194.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
